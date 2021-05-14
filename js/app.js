245785006de6d06e2aaad580cf821b6ca3f7f9dc
SimonaRoxanaNegri <84034489+SimonaRoxanaNegri@users.noreply.github.com>
2021-05-14 19:25:26 +0200

    aggiunti pulsanti con js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
+/* Bottone incrementa */
 
-document.getElementById("add").addEventListener("click", add)
-document.getElementById("decrease").addEventListener("click", decrease)
-//valore del count 
+let btnIncrement = document.createElement("BUTTON");
+let divButton = document.getElementById("buttons")
+btnIncrement.setAttribute('content', 'test content');
+btnIncrement.setAttribute('class', 'btn-add');
+btnIncrement.textContent = 'incrementa +';
+divButton.appendChild(btnIncrement).addEventListener("click", add);
+
+/* Bottone decrementa */
+
+let btnDecrease = document.createElement("BUTTON");
+let divButton2 = document.getElementById("buttons")
+btnDecrease.setAttribute('content', 'test content');
+btnDecrease.setAttribute('class', 'btn-decrease');
+btnDecrease.textContent = 'diminuisci -';
+divButton.appendChild(btnDecrease).addEventListener("click", decrease);
+
+
+let valueCounter = document.getElementById("count")
 let count = 0
-
+valueCounter.innerHTML = count
 function add() {
-    return document.getElementById("count").innerHTML = count++
+    return document.getElementById("count").innerHTML = ++count
 }
 function decrease() {
-    return document.getElementById("count").innerHTML = --count
+    if (count > 0)
+        return document.getElementById("count").innerHTML = --count
 }
 
