dab8f5459d99f19153908e5c90c20b936c01971c
SimonaRoxanaNegri <84034489+SimonaRoxanaNegri@users.noreply.github.com>
2021-05-15 09:13:13 +0200

    modifiche css, javascript

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,8 @@
 let btnIncrement = document.createElement("BUTTON");
 let divButton = document.getElementById("buttons")
 btnIncrement.setAttribute('content', 'test content');
-btnIncrement.setAttribute('class', 'btn-add');
-btnIncrement.textContent = 'incrementa +';
+btnIncrement.setAttribute('class', 'btn-custom');
+btnIncrement.textContent = 'incrementa';
 divButton.appendChild(btnIncrement).addEventListener("click", add);
 
 /* Bottone decrementa */
@@ -12,19 +12,32 @@ divButton.appendChild(btnIncrement).addEventListener("click", add);
 let btnDecrease = document.createElement("BUTTON");
 let divButton2 = document.getElementById("buttons")
 btnDecrease.setAttribute('content', 'test content');
-btnDecrease.setAttribute('class', 'btn-decrease');
-btnDecrease.textContent = 'diminuisci -';
-divButton.appendChild(btnDecrease).addEventListener("click", decrease);
+btnDecrease.setAttribute('class', 'btn-custom');
+btnDecrease.textContent = 'diminuisci';
+divButton2.appendChild(btnDecrease).addEventListener("click", decrease);
 
+/* Bottone reset */
 
-let valueCounter = document.getElementById("count")
+let btnReset = document.createElement("BUTTON");
+let divButton3 = document.getElementById("buttons")
+btnReset.setAttribute('content', 'test content');
+btnReset.setAttribute('class', 'btn-custom');
+btnReset.textContent = 'reset';
+divButton3.appendChild(btnReset).addEventListener("click", reset);
+
 let count = 0
+let valueCounter = document.getElementById("count")
 valueCounter.innerHTML = count
+
 function add() {
-    return document.getElementById("count").innerHTML = ++count
+    document.getElementById("count").innerHTML = ++count
+
 }
 function decrease() {
     if (count > 0)
-        return document.getElementById("count").innerHTML = --count
+        document.getElementById("count").innerHTML = --count
 }
-
+function reset() {
+    count = 0
+    document.getElementById("count").innerHTML = count;
+}
