960c1b7e22ba439bb22e77d2db125b2d9ed56340
SimonaRoxanaNegri <84034489+SimonaRoxanaNegri@users.noreply.github.com>
2021-05-17 12:32:57 +0200

    aggiunto alert

--- js/app.js
+++ js/app.js
@@ -3,28 +3,56 @@
 let btnIncrement = document.createElement("BUTTON");
 let divButton = document.getElementById("buttons")
 btnIncrement.setAttribute('content', 'test content');
-btnIncrement.setAttribute('class', 'btn-add');
-btnIncrement.textContent = 'incrementa +';
+btnIncrement.setAttribute('class', 'btn-custom');
+btnIncrement.textContent = 'incrementa';
 divButton.appendChild(btnIncrement).addEventListener("click", add);
 
 /* Bottone decrementa */
 
-let btnDecrease = document.createElement("BUTTON");
+let btnDecrease = document.createElement("BUTTON")
 let divButton2 = document.getElementById("buttons")
-btnDecrease.setAttribute('content', 'test content');
-btnDecrease.setAttribute('class', 'btn-decrease');
-btnDecrease.textContent = 'diminuisci -';
-divButton.appendChild(btnDecrease).addEventListener("click", decrease);
+btnDecrease.setAttribute('content', 'test content')
+btnDecrease.setAttribute('class', 'btn-custom')
+btnDecrease.textContent = 'diminuisci'
+divButton2.appendChild(btnDecrease).addEventListener("click", decrease)
+
+/* Bottone reset */
+
+let btnReset = document.createElement("BUTTON")
+let divButton3 = document.getElementById("buttons")
+btnReset.setAttribute('content', 'test content')
+btnReset.setAttribute('class', 'btn-custom')
+btnReset.textContent = 'reset'
+divButton3.appendChild(btnReset).addEventListener("click", reset)
 
 
-let valueCounter = document.getElementById("count")
 let count = 0
+let valueCounter = document.getElementById("count")
 valueCounter.innerHTML = count
+
 function add() {
-    return document.getElementById("count").innerHTML = ++count
-}
-function decrease() {
-    if (count > 0)
-        return document.getElementById("count").innerHTML = --count
+    document.getElementById("count").innerHTML = ++count
 }
 
+/* ALERT */
+
+let divAlert = document.createElement("div")
+let alertCustom = document.getElementById("alert")
+alertCustom.appendChild(divAlert).addEventListener("click", decrease)
+
+function decrease() {
+    if (count > 0) {
+        document.getElementById("count").innerHTML = --count
+    }
+    else {
+        document.getElementById("alert").innerHTML = `<div class="alert alert-custom alert-dismissible show alert-custom" role="alert">
+        <strong>Attenzione!</strong> Sotto lo zero non è consentito andare.
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>`
+    }
+
+}
+function reset() {
+    count = 0
+    document.getElementById("count").innerHTML = count
+}
